44c3bb873e8c3f9dfb6035996854cf8616ffaaf1
Van Aru <vanearu423@gmail.com>
2025-05-17 19:11:20 -0300

    actualizado

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,5 @@
-alert("")
-
-// comentario de línea
-
-/* comentario
-de bloque
-*/
+const nombreProducto = prompt("¿Cuál es el nombre del producto?");
+const precioUnitario = 40;
+const cantidadDeseada = prompt(`El precio del ${nombreProducto} es de $${precioUnitario}, Que cantidad desea comprar del producto?`);
+let costoTotal = cantidadDeseada*precioUnitario;
+alert(`el costo total es de ${costoTotal}`)
